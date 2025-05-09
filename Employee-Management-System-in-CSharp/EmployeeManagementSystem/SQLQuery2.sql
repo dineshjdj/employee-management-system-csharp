a10a6f058cffaf06a7f94e3c0be3f16b70f98f03
dineshjdj <159233668+dineshjdj@users.noreply.github.com>
2025-05-09 21:11:43 +0530

    Add files via upload
    A Windows Forms Application (WinForms) built in C# that manages employee records, tracks active/inactive employees, handles salary updates, and provides a real-time dashboard.

---
+++ Employee-Management-System-in-CSharp/EmployeeManagementSystem/SQLQuery2.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE employees 
+(
+id INT PRIMARY KEY IDENTITY(1,1),
+employee_id VARCHAR (MAX) NULL,
+full_name VARCHAR (max) NULL,
+gender VARCHAR (max) NULL,
+contact_number VARCHAR (max) NULL,
+position VARCHAR (max) NULL,
+image VARCHAR (max) NULL,
+salary INT NULL,
+insert_date DATE NULL,
+update_date DATE NULL,
+delete_date DATE NULL,
+)
+
+SELECT * FROM employees
+
+ALTER TABLE employees
+ADD status VARCHAR (MAX)
